0b9c6140637b64d3a0d54f6f4b23c1be3aa48f03
Tomas Jancik <t.jancik@gmail.com>
2015-12-01 23:29:15 +0100

    fix base url detection if in subfolder

--- bootstrap.php
+++ bootstrap.php
@@ -8,13 +8,21 @@ define('_LIBS_', _BASEPATH_ . 'libs/');
 define('_TMP_', _BASEPATH_ . 'tmp/');
 define('_CONTROLLERS_', _BASEPATH_ . 'controllers/');
 
-define('_BASEURL_', $_SERVER['SERVER_NAME']);
+// get base URL, without subdir if exists
+$base = $_SERVER['PHP_SELF'];
+$idx = strrpos($base, '/');
+define('_BASEURL_', substr($_SERVER['PHP_SELF'], 0, $idx));
+
+// get URI without base path
+$idx = strpos($_SERVER['REQUEST_URI'], _BASEURL_) + strlen(_BASEURL_);
+$uri = substr($_SERVER['REQUEST_URI'], $idx);
+unset($base, $idx);
 
 $refreshAutoloaderCache = true;
 require_once _LIBS_ . 'autoloader.php';
 autoloader::init($refreshAutoloaderCache);
 
-$router = new router($_SERVER['REQUEST_URI']);
+$router = new router($uri);
 
 // load the controller
 $controller = $router->getController();
